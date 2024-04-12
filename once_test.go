38740d802b422d864f35c2aae1f48ad1b236e3e1
mind <peace0phmind@gmail.com>
2024-04-12 16:51:15 +0800

    add timeout once

--- once_test.go
+++ once_test.go
@@ -1,7 +1,19 @@
 package sync
 
-import "testing"
+import (
+	"context"
+	"github.com/stretchr/testify/assert"
+	"testing"
+	"time"
+)
 
 func TestTimeout(t *testing.T) {
+	once := Once{}
 
+	err := once.DoTimeout(100*time.Millisecond, func() error {
+		time.Sleep(200 * time.Millisecond)
+		return nil
+	})
+
+	assert.Equal(t, context.DeadlineExceeded, err)
 }
